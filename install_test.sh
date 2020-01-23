049c179b5dea350f9543993210a6bc32b875f98b
CryptoRaptor <31970407+cryptopoly@users.noreply.github.com>
2020-01-23 10:22:22 +0000

    Create install_test.sh

---
+++ install_test.sh
@@ -0,0 +1,8 @@
+sudo apt install curl git autoconf
+sudo apt-get install autoconf libtool pkg-config
+sudo apt-get install libboost-all-dev libssl-dev libevent-dev
+sudo apt install linuxbrew-wrapper
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+brew install bitcoin
+git clone https://github.com/commerceblock/ocean
+cd $HOME/ocean
