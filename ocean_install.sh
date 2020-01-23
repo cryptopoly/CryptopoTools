7e9a96b859870a3f05b5df3f84a64d4dfc969e5c
CryptoRaptor <31970407+cryptopoly@users.noreply.github.com>
2020-01-23 09:12:28 +0000

    Update ocean_install.sh

--- ocean_install.sh
+++ ocean_install.sh
@@ -1,16 +1,12 @@
-Ocean - Linx Installation (https://github.com/goldtokensa) [+windows linux subsystem]
-
-
 # Log
 set -x
 # Ocean
 cd $HOME
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt install git curl autoconf libtool pkg-config libboost-all-dev libdb4.8-dev libdb4.8++-dev -dist openssl -libssl-dev libevent-dev python3-pip -y
-git clone https://github.com/commerceblock/ocean
-cd $HOME/ocean
 sudo apt update -y
 sudo apt dist-upgrade -y
+sudo apt install git -y
+sudo apt install curl -y
 sudo apt install autoconf -y
 sudo apt install libtool -y
 sudo apt install pkg-config -y
@@ -21,6 +17,8 @@ sudo apt install libssl-dev -y
 sudo apt install libevent-dev -y
 sudo apt install python3-pip -y
 sudo apt autoremove -y
+git clone https://github.com/commerceblock/ocean
+cd $HOME/ocean
 ./autogen.sh
 ./configure
 make
