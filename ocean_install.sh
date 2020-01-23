b206ed046c9496cda087b763b47f468ad0187ff2
CryptoRaptor <31970407+cryptopoly@users.noreply.github.com>
2020-01-23 08:57:54 +0000

    Update ocean_install.sh

--- ocean_install.sh
+++ ocean_install.sh
@@ -1,17 +1,29 @@
+Ocean - Linx Installation (https://github.com/goldtokensa) [+windows linux subsystem]
+
+
 # Log
 set -x
-# Curl - Linux only
-sudo apt install curl -y
-# Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Ocean
-brew reinstall git python python3 amap automake berkeley-db4 libtool boost miniupnpc openssl pkg-config protobuf qt5 libevent autoconf openssl
 cd $HOME
-git clone https://github.com/commerceblock/ocean.git
-cd ocean
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt install git curl autoconf libtool pkg-config libboost-all-dev libdb4.8-dev libdb4.8++-dev -dist openssl -libssl-dev libevent-dev python3-pip -y
+git clone https://github.com/commerceblock/ocean
+cd $HOME/ocean
+sudo apt update -y
+sudo apt dist-upgrade -y
+sudo apt install autoconf -y
+sudo apt install libtool -y
+sudo apt install pkg-config -y
+sudo apt install libboost-all-dev -y
+sudo apt install libdb4.8-dev libdb4.8++-dev -y 
+sudo apt install -dist openssl -y
+sudo apt install libssl-dev -y
+sudo apt install libevent-dev -y
+sudo apt install python3-pip -y
+sudo apt autoremove -y
 ./autogen.sh
 ./configure
 make
 make -j4
 cd $HOME
-mkdir ocean_mainnet
+mkdir ocean_main
