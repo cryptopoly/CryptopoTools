2adec984bb2d876ad4412a05e4283057d887a3b4
CryptoRaptor <dan.eve@hotmail.com>
2020-02-06 19:14:39 +0000

    Ocean Node - CBT Scripts

---
+++ CBT_Node_Install_Script_MacOS.sh
@@ -0,0 +1,30 @@
+# Step 1 - Open terminal
+# Step 2 - Type 'nano cbt.sh' and press Enter [nano text editor appears]
+# Step 3 - Copy below script into nano
+# Step 4 - Press Ctrl+x
+# Step 5 - Type 'y'
+# Step 6 - Press Enter to save
+# Step 7 - Type 'chmod +x cbt.sh' and press Enter
+# Step 8 - Type './cbt.sh' and press Enter
+# Step 9 - Enjoy!
+
+
+#### COPY BELOW INTO NANO ###
+
+# Log stuff
+set -x
+# Install stuff
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo apt install git -y
+git clone https://github.com/commerceblock/config
+sudo apt install docker -y
+sudo apt install docker-compose -y
+sudo apt autoremove -y
+mv config ocean
+cd ocean/mainnet/docker
+
+# Run Stuff
+sudo docker-compose -p cbt up -d
+sleep 10
+sudo docker exec cbt_ocean_1 ocean-cli -rpcport=8332 -rpcuser=ocean -rpcpassword=oceanpass getblockchaininfo
